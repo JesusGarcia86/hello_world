87b0c392928c862511b48f62ca7ef0196e4fcd87
JesusGarcia86 <the_sampritas@hotmail.com>
2020-12-26 10:47:56 -0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+name = "Jesus"
+print("Hello", name)
+print("Hello "+name)
+
+name = 7
+print("Hello", name)
+print("Hello "+str(name))
+
+fave_food1 = "burgers"
+fave_food2 = "chilaquiles"
+print("I love to eat {} and {}".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
